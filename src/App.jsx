c8cce25ef15bba9dab76a40a144784aeb7d6e1ef
robcabobb <robcabobb@gmail.com>
2022-04-28 11:34:29 -0400

    added comp files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from '../src/components/header/Header'
+import About from './components/'
+import Contact from './components/'
+import Experience from './components/'
+import Footer from './components/'
+import Nav from './components/'
+import Portfolio from './components/'
+import Services from './components/'
+import Testimonials from './components/'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    
+    </>
   )
 }
 
