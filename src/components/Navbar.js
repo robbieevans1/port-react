ebee502cb7d282c3bd3ca700fde703addddec539
robcabobb <robcabobb@gmail.com>
2022-04-29 19:06:20 -0400

    items for navbar are displaying

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,42 @@
 import React from "react";
-import {FaBars, FaTimes} from 'react-icons/fa'
-import Logo from '../assets/logo.png'
-
+import { FaBars, FaTimes } from "react-icons/fa";
+import Logo from "../assets/robbie.png";
 
 const Navbar = () => {
 	return (
-		<div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-			<div>
-			<img src={Logo} alt="Logo"></img>
+		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+			<div >
+				<img className="mt-5" src={Logo} alt="Logo" style={{ width: "500px" }}></img>
 			</div>
-    </div>
+
+			{/* menu */}
+			<div className="hidden md:flex">
+				<ul className="flex">
+					<li>Home</li>
+					<li>About</li>
+					<li>Skillset</li>
+					<li>Work</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+
+			{/* Hamburger Button */}
+			<div className="hidden">
+				<FaBars />
+			</div>
+
+			{/* Mobile menu */}
+			<ul className="hidden">
+				<li>Home</li>
+				<li>About</li>
+				<li>Skillset</li>
+				<li>Work</li>
+				<li>Contact</li>
+			</ul>
+
+			{/* Social buttons */}
+			<div className="hidden"></div>
+		</div>
 	);
 };
 
