cb907675b2a5d2f3d5ab050366c66c43e8c3261c
robcabobb <robcabobb@gmail.com>
2022-05-03 13:53:19 -0400

    added custom styling, started workk on work cards

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,36 +1,40 @@
-import React from 'react'
+import React from "react";
+import Feast from "../assets/work-imgs/feast.png";
+import PizzaHunt from "../assets/work-imgs/pizzaHunt.png";
 
 const Work = () => {
-  return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-          <p className='py-6'>Here are some of my recently completed projects:</p>
-        </div>
-        
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div>
-            <div>
-              <div>
-                <span></span>
-                <div>
-                  <a href='/'>
-                    <button></button>
-                  </a>
-                  <a href='/'>
-                    <button></button>
-                  </a>
+	return (
+		<div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+				<div className="pb-8">
+					<p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+						Work
+					</p>
+					<p className="py-6">
+						Here are some of my recently completed projects:
+					</p>
+				</div>
 
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+				<div style={{backgroundImage: `url(${Feast})`}} className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+					<div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+					
+          	{/* Hover */}
+						<div>
+							<span></span>
+							<div>
+								<a href="/">
+									<button></button>
+								</a>
+								<a href="/">
+									<button></button>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-    </div>
-  )
-}
-
-export default Work
+export default Work;
