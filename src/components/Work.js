dbbe4d843b0500a8ca5e333ca2bca1d7970436f8
robcabobb <robcabobb@gmail.com>
2022-05-03 14:10:35 -0400

    started adding work cards

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Feast from "../assets/work-imgs/feast.png";
 import PizzaHunt from "../assets/work-imgs/pizzaHunt.png";
+import WeatherDashboard from '../assets/work-imgs/weatherDashboard.png'
 
 const Work = () => {
 	return (
@@ -15,22 +16,121 @@ const Work = () => {
 					</p>
 				</div>
 
-				<div style={{backgroundImage: `url(${Feast})`}} className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-					<div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+				<div  className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* item 1 */}
+        	<div style={{backgroundImage: `url(${PizzaHunt})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
 					
           	{/* Hover */}
-						<div>
-							<span></span>
-							<div>
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider">
+                HTML, CSS and JavaScript App
+              </span>
+							<div className="pt-8 text-center">
 								<a href="/">
-									<button></button>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Deployment</button>
 								</a>
 								<a href="/">
-									<button></button>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Repo</button>
 								</a>
 							</div>
 						</div>
 					</div>
+
+          {/* item 2 */}
+					<div style={{backgroundImage: `url(${Feast})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+					
+          	{/* Hover */}
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider">
+                HTML, CSS and JavaScript App
+              </span>
+							<div className="pt-8 text-center">
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Deployment</button>
+								</a>
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Repo</button>
+								</a>
+							</div>
+						</div>
+					</div>
+
+          {/* item 3 */}
+					<div style={{backgroundImage: `url(${WeatherDashboard})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+					
+          	{/* Hover */}
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider">
+                HTML, CSS and JavaScript App
+              </span>
+							<div className="pt-8 text-center">
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Deployment</button>
+								</a>
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Repo</button>
+								</a>
+							</div>
+						</div>
+					</div>
+
+          {/* item 4 */}
+					<div style={{backgroundImage: `url(${PizzaHunt})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+					
+          	{/* Hover */}
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider">
+                HTML, CSS and JavaScript App
+              </span>
+							<div className="pt-8 text-center">
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Deployment</button>
+								</a>
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Repo</button>
+								</a>
+							</div>
+						</div>
+					</div>
+
+          {/* item 5 */}
+					<div style={{backgroundImage: `url(${PizzaHunt})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+					
+          	{/* Hover */}
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider">
+                HTML, CSS and JavaScript App
+              </span>
+							<div className="pt-8 text-center">
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Deployment</button>
+								</a>
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Repo</button>
+								</a>
+							</div>
+						</div>
+					</div>
+
+          {/* item 6 */}
+					<div style={{backgroundImage: `url(${PizzaHunt})`}} className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+					
+          	{/* Hover */}
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider">
+                HTML, CSS and JavaScript App
+              </span>
+							<div className="pt-8 text-center">
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Deployment</button>
+								</a>
+								<a href="/">
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Repo</button>
+								</a>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
