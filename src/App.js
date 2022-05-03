9ed0c48994fe79c1264a4d42dfa8ecea1bbc6204
robcabobb <robcabobb@gmail.com>
2022-05-03 13:15:51 -0400

    finished tech skills used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
+      <Work />
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,7 +5,6 @@ import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
 import AWS from '../assets/aws.png';
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
@@ -13,7 +12,53 @@ import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name='skills' className=' w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div>
+
+      </div>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+    <div>
+      <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Tech Skills</p>
+      <p className='py-4'>Here's the tech that I use most frequently</p>
+    </div>
+
+    <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={HTML} alt='html icon'></img>
+        <p className='my-4'>HTML</p>
+      </div>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={CSS} alt='html icon'></img>
+        <p className='my-4'>CSS</p>
+      </div>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={JavaScript} alt='html icon'></img>
+        <p className='my-4'>JavaScript</p>
+      </div>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={ReactImg} alt='html icon'></img>
+        <p className='my-4'>React</p>
+      </div>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={GitHub} alt='html icon'></img>
+        <p className='my-4'>GitHub</p>
+      </div>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={Node} alt='html icon'></img>
+        <p className='my-4'>Node.js</p>
+      </div>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={Mongo} alt='html icon'></img>
+        <p className='my-4'>MongoDB</p>
+      </div>
+      <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+        <img className='w-20 mx-auto' src={AWS} alt='html icon'></img>
+        <p className='my-4'>AWS</p>
+      </div>
+    </div>
+      </div>  </div>
+    
   )
 }
 
