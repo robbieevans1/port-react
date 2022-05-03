7611b2e439819be0944fca94b34465d80faf7940
robcabobb <robcabobb@gmail.com>
2022-05-03 15:21:27 -0400

    refactored nav

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form action='' className='flex flex-col max-w-[600px] w-full'>
+      <form method='post' action='https://getform.io/f/50211ab8-8c61-4baf-ad38-480ae3bea580' className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
           <p className='text-gray-300 py-4'> Please you the form below to contact me or shoot me an email at robert.m.evans3@gmail.com</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
 	return (
 		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
 			<div className=" ">
-				<img src={Logo} alt="Logo" style={{ width: "50px" }}></img>
+				{/* <img src={Logo} alt="Logo" style={{ width: "50px" }}></img> */}
 			</div>
 
 			{/* menu */}
@@ -54,14 +54,38 @@ const Navbar = () => {
 				className={
 					!nav
 						? "hidden"
-						: "absolute top-0 left-0 h-screen w-full bg-[#0a192f] flex flex-col justify-center items-center"
+						: "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
 				}
 			>
-				<li className="py-6 text-4xl">Home</li>
-				<li className="py-6 text-4xl">About</li>
-				<li className="py-6 text-4xl">Skillset</li>
-				<li className="py-6 text-4xl">Work</li>
-				<li className="py-6 text-4xl">Contact</li>
+				<li className="py-6 text-4xl">
+					<Link onClick={handleClick} to="home" smooth={true} duration={500}>
+						Home
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					{" "}
+					<Link onClick={handleClick} to="about" smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					{" "}
+					<Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+						Skills
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					{" "}
+					<Link onClick={handleClick} to="work" smooth={true} duration={500}>
+						Work
+					</Link>
+				</li>
+				<li className="py-6 text-4xl">
+					{" "}
+					<Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+						Contact
+					</Link>
+				</li>
 			</ul>
 
 			{/* Social buttons */}
