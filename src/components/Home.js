1423fffa309f160b75a603742f569fb9e805e84d
robcabobb <robcabobb@gmail.com>
2022-05-08 00:17:47 -0400

    edited button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
-  return (
-    <div className='w-full h-screen bg-[#5cdb94]'>
-      {/* Container */}
-      <div className='max-w-[1000px] mx-auto px-B flex flex-col justify-center h-full'>
-        <p className='text-blue-900'>Hello! my name is</p>
-        <h1 className='text-4xl sm:text-6xl font-bold text-blue-900'>Robbie Evans</h1>
-        <h2 className='text-4xl sm:text-6xl font-bold text-blue-900'>I am a junior web developer.</h2>
-        <h2 className='py-4 max-w-[700px] text-blue-900'>I am currently enrolled in University of Richmond's Coding Bootcamp.</h2>
-      <div>
-        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-900 hover:border-blue-900'>View Work
-        <span className='group-hover:rotate-90 duration-300'>
-        <HiArrowNarrowRight className='ml-3' />
-        </span>
-          
-        </button>
-      </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div name="home" className="w-full h-screen bg-[#5cdb94]">
+			{/* Container */}
+			<div className="max-w-[1000px] mx-auto px-B flex flex-col justify-center h-full">
+				<p className="text-blue-900">Hello! my name is</p>
+				<h1 className="text-4xl sm:text-6xl font-bold text-blue-900">
+					Robbie Evans
+				</h1>
+				<h2 className="text-4xl sm:text-6xl font-bold text-blue-900">
+					I am a junior web developer.
+				</h2>
+				<h2 className="py-4 max-w-[700px] text-blue-900">
+					I am currently enrolled in University of Richmond's Coding Bootcamp.
+				</h2>
+				<div>
+					<button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-900 hover:border-blue-900">
+						{" "}
+						<Link to="work" smooth={true} duration={500}>
+							View Work
+						</Link>
+						<span className="group-hover:rotate-90 duration-300">
+							<HiArrowNarrowRight className="ml-3" />
+						</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
